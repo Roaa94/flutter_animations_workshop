86967dfcf57c065c7326d481690838cf2439d3eb
roaa94 <roaa94khaddam@gmail.com>
2022-10-21 22:52:24 +0300

    set up :construction:

---
+++ android/app/src/main/kotlin/com/roaakdm/flutter/animations/workshop/flutter_animations_workshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.roaakdm.flutter.animations.workshop.flutter_animations_workshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+List<Color> colors = [
+  Colors.purple,
+  Colors.yellow,
+  Colors.greenAccent,
+  Colors.blueAccent,
+  Colors.deepPurple,
+  Colors.redAccent,
+  Colors.green.shade900,
+];
