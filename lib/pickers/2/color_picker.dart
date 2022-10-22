52eb45255f9f9fc4bcb81b05e45aa20d07e0e75f
roaa94 <roaa94khaddam@gmail.com>
2022-10-22 08:49:39 +0300

    fix: fix import

--- lib/pickers/2/color_picker.dart
+++ lib/pickers/2/color_picker.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_animations_workshop/colors.dart';
-import 'package:flutter_animations_workshop/pickers/1/color_item.dart';
+import 'package:flutter_animations_workshop/pickers/2/color_item.dart';
 
 class ColorPicker extends StatefulWidget {
   const ColorPicker({super.key});
